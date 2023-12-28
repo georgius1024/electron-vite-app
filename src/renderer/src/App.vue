80e77bed904a8a65553bc8865b550f1558b4d1fc
georgius1024 <georgius@ural.ru>
2023-12-28 19:07:17 +0500

    preload

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+const readDir = window.api.readDir
+
+const dir = ref([])
+
+const scanDir = () => {
+  dir.value = readDir('.')
+}
+</script>
+
+<template>
+  <main class="container">
+    <button @click="scanDir">Scan dir</button>
+    <ul>
+      <li v-for="file in dir" :key="file">
+        {{ file }}
+      </li>
+    </ul>
+  </main>
+</template>
